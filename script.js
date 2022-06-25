d97825062521ddca921bfc95ca7fa1bbb3be2b14
Yethu <yethukichu55@gmail.com>
2022-06-25 21:17:14 +0530

    r-p-s

--- script.js
+++ script.js
@@ -7,41 +7,71 @@ return items[Math.floor(Math.random()*items.length)];
 function play(cs, ps){
    if(cs==="rock" && ps==="paper"){
        alert("you win");
+       var div = document.getElementById('p1');
+       div.innerHTML += '1';
    }
    else if(cs==="rock" && ps==="scissor"){
        alert("you lose");
+       var div = document.getElementById('p2');
+
+       div.innerHTML += '1';
    }
    else if(cs==="rock" && ps==="rock"){
        alert("tie");
+      
    }
    else if(cs==="paper" && ps==="scissor"){
        alert("you win");
+       var div = document.getElementById('p1');
+       div.innerHTML += '1';
    }
    else if(cs==="paper" && ps==="rock"){
        alert("you lose");
+       var div = document.getElementById('p2');
+       div.innerHTML += '1';
    }
    else if(cs==="paper" && ps==="paper"){
        alert("tie");
+      
    }
    else if(cs==="scissor" && ps==="scissor"){
        alert("tie");
+       
    }
    else if(cs==="scissor" && ps==="rock"){
        alert("you win");
+       var div = document.getElementById('p1');
+       div.innerHTML += '1';
    }
    else if(cs==="scissor" && ps==="paper"){
        alert("you lose");
+       var div = document.getElementById('p2');
+       div.innerHTML += '1';
    }
 }
-function game(){
-   for (let i = 0; i < 5; i++) {
+
+let cs=computerplay();
+let ps=("enter ur choice").toLowerCase();
+   
+      
+      
     
-    let cs=computerplay();
-    let ps=prompt("enter ur choice").toLowerCase();
-    play(cs,ps);
-      
-      
-       
-   }
+
+const btn = document.querySelectorAll('div.content button');
+
+btn.forEach((btn) => {
+btn.addEventListener('click',function(e){
+if(e.target.className==='rock' || e.target.className==='paper' ||e.target.className==='scissor'){
+console.log(e.target.className)
+ps = e.target.className;
+
 }
-game();
+let cs=computerplay();
+play(cs,ps);
+var text = document.getElementById('p1').innerHTML;
+console.log(text);
+});
+
+});
+
+
