cf57feae71771c6f596c0751c845bacae82a0954
Yethu <yethukichu55@gmail.com>
2022-06-26 22:33:25 +0530

    basic design rock-paper-scissor

--- script.js
+++ script.js
@@ -1,58 +1,78 @@
+let player = 0;
+let computer = 0;
+
 let items=["rock","paper","scissor"];
 function computerplay(){
 return items[Math.floor(Math.random()*items.length)];
 }
+const container = document.querySelector('#para');
+const win=document.querySelector('#win');
+container.textContent = "you :"+player+" "+"computer :"+computer;
+let count=0;
+
+
 
 
 function play(cs, ps){
    if(cs==="rock" && ps==="paper"){
        alert("you win");
-       var div = document.getElementById('p1');
-       div.innerHTML += '1';
+       player+=1;
+    container.textContent = "you :"+player+" "+"computer :"+computer;
+    count++;
+       
    }
    else if(cs==="rock" && ps==="scissor"){
        alert("you lose");
-       var div = document.getElementById('p2');
-
-       div.innerHTML += '1';
+       computer+=1;
+       container.textContent = "you :"+player+" "+"computer :"+computer;
+       count++;
    }
    else if(cs==="rock" && ps==="rock"){
        alert("tie");
-      
+       count++;
    }
    else if(cs==="paper" && ps==="scissor"){
        alert("you win");
-       var div = document.getElementById('p1');
-       div.innerHTML += '1';
+       player+=1;
+       container.textContent = "you :"+player+" "+"computer :"+computer;
+       count++;
    }
    else if(cs==="paper" && ps==="rock"){
        alert("you lose");
-       var div = document.getElementById('p2');
-       div.innerHTML += '1';
+       computer+=1;
+       container.textContent = "you :"+player+" "+"computer :"+computer;
+       count++;
    }
    else if(cs==="paper" && ps==="paper"){
        alert("tie");
+       count++;
       
    }
    else if(cs==="scissor" && ps==="scissor"){
        alert("tie");
+       count++;
        
    }
    else if(cs==="scissor" && ps==="rock"){
        alert("you win");
-       var div = document.getElementById('p1');
-       div.innerHTML += '1';
+       player+=1;
+       container.textContent = "you :"+player+" "+"computer :"+computer;
+       count++;
+       
    }
    else if(cs==="scissor" && ps==="paper"){
        alert("you lose");
-       var div = document.getElementById('p2');
-       div.innerHTML += '1';
+       computer+=1;
+       container.textContent = "you :"+player+" "+"computer :"+computer;
+       count++;
    }
 }
 
 let cs=computerplay();
 let ps=("enter ur choice").toLowerCase();
    
+const div = document.querySelector('#win'); 
+div.setAttribute('style', 'color: blue; font-size:50px; ');  
       
       
     
@@ -66,12 +86,32 @@ console.log(e.target.className)
 ps = e.target.className;
 
 }
-let cs=computerplay();
-play(cs,ps);
-var text = document.getElementById('p1').innerHTML;
-console.log(text);
-});
+if(count<5){
+    let cs=computerplay();
+    play(cs,ps);
+}
+else{
+    checkwin();
+}
+
 
 });
 
+});
+function checkwin(){
+    if(player > computer){
+        win.textContent = "you win";
+    }
+    else if(player < computer)
+        win.textContent = "you lose";
+    
+}
 
+
+
+
+
+    
+
+    
+    
